59d57b821321028e9301117273b883ec2471c9f9
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-04-30 15:08:02 +0200

    Comments

--- b/Stock.hs
+++ b/Stock.hs
@@ -8,13 +8,16 @@ import Prelude hiding (zipWith)
 main :: IO ()
 main = undefined
 
-type BuyDay = Int
-type SellDay = Int
 
 test :: Array U DIM1 Int
 test = fromListUnboxed (Z :. 8) [0,0,2,9,8,10,1,10]
 
-buildMatrix :: Array U DIM1 Int -> Array D DIM2 (BuyDay, SellDay, Int)
+test2 :: Array U DIM1 Int
+test2 = let l = [12, 11, 10, 8, 5, 8, 9, 6, 7, 7, 10, 7, 4, 2]
+        in fromListUnboxed (Z :. length l) l
+
+-- | Build matrix of all possible (buy, sell, profit) tuples
+buildMatrix :: Array U DIM1 Int -> Array D DIM2 (Int, Int, Int)
 buildMatrix inp =
   traverse inp
            (\(Z :. x) -> ix2 x x)
@@ -25,9 +28,10 @@ buildMatrix inp =
                       ,s
                       ,idx (ix1 s) - idx (ix1 b)))
 
+-- | For each buy day find the earliest sell day with largest profit
 getSell :: (Monad m)
-        => Array D DIM2 (BuyDay,SellDay,Int)
-        -> m (Array U DIM1 (BuyDay,SellDay,Int))
+        => Array D DIM2 (Int,Int,Int)
+        -> m (Array U DIM1 (Int,Int,Int))
 getSell inp =
   foldP (\(b1,s1,p1) (b2,s2,p2) ->
            if p1 >= p2 -- Pick the earlier sell days
@@ -36,6 +40,7 @@ getSell inp =
         (0,0,0)
         inp
 
+-- | For find the best and latest day to buy
 getBuy inp =
   foldP (\(b1,s1,p1) (b2,s2,p2) ->
            if p1 > p2 -- pick the later buy days
@@ -44,7 +49,8 @@ getBuy inp =
         (0,0,0)
         inp
 
-buySell :: Monad m => Array U DIM1 Int -> m (BuyDay, SellDay, Int)
+-- | Solve the problem
+buySell :: Monad m => Array U DIM1 Int -> m (Int, Int, Int)
 buySell prices = do let mat = buildMatrix prices
                     foundSell <- getSell mat
                     foundBuy <- getBuy foundSell
