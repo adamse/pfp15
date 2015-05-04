8721ada7955b392bb359eb2727837b12b80cc86e
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-05-04 18:30:43 +0200

    finish gaussian example

--- b/RepaFilter.hs
+++ b/RepaFilter.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE RankNTypes #-}
 -- |
 
 module RepaFilter where
 
-import Prelude hiding (map)
+import           Prelude hiding (map)
+
+import Control.Applicative
 
 -- from repa
-import Data.Array.Repa
-import Data.Array.Repa.Stencil
-import Data.Array.Repa.Specialised.Dim2
+import           Data.Array.Repa
+import           Data.Array.Repa.Stencil
+import Data.Array.Repa.Stencil.Dim2
+import           Data.Array.Repa.Specialised.Dim2
+import qualified Data.Array.Repa.Repr.Unboxed as U
+
 
 -- from repa-io
-import Data.Array.Repa.IO.BMP
-import Data.Word (Word8)
+import           Data.Array.Repa.IO.BMP
+import           Data.Word (Word8)
 
 
 main :: IO ()
@@ -39,11 +45,41 @@ doubleToBmp image =
                 image)
   where toWord c = round (c * 255)
 
+inside :: Int -> DIM2 -> Bool
+inside r (Z :. x :. y) = abs x <= r && abs y <= y
+{-# INLINE inside #-}
+
+rToExtent :: Int -> DIM2
+rToExtent r = ix2 rr rr
+  where rr = 2 * r + 1
+{-# INLINE rToExtent #-}
+
+-- applyFilter :: (forall m. Monad m => Array U DIM2 (Double,Double,Double) -> m (Array U DIM2 (Double,Double,Double)))
+--             -> FilePath
+--             -> FilePath
+--             -> IO ()
+applyFilter filter inFile outFile =
+  do im <-
+       fmap (either (error . show) id)
+            (readImageFromBMP inFile)
+     im' <- bmpToDouble im
+     filtered <- filter im'
+     outIm <- doubleToBmp filtered
+     writeImageToBMP outFile outIm
+
 -- | using luminance preserving greyscale conversion, a simple filter
 greyscale :: Monad m => Array U DIM2 (Double,Double,Double) -> m (Array U DIM2 Double)
 greyscale image =
   computeP (map (\(r,g,b) -> 0.2126 * r + 0.7152 * g + 7.22e-2 * b) image)
 
+applyGreyscale :: IO ()
+applyGreyscale =
+  applyFilter
+    (\im ->
+       do gim <- greyscale im
+          return (U.zip3 gim gim gim))
+    "FLAG_B24.BMP"
+    "FLAG_B24_GREY.BMP"
 
 -- * Gaussian blur
 
@@ -52,13 +88,12 @@ greyscale image =
 simpleBlurStencil :: Int -> Stencil DIM2 Double
 simpleBlurStencil r =
   makeStencil
-    extent
+    (rToExtent r)
     (\ix ->
-       if isInside2 extent ix
+       if inside r ix
           then Just coeff
           else Nothing)
   where coeff = recip (fromIntegral r ** 2)
-        extent = ix2 r r
 
 
 {-
@@ -70,12 +105,43 @@ apply a gaussian blur to an image we use a Repa construct called a `Stencil`, a
 gaussianStencil :: Double -> Int -> Stencil DIM2 Double
 gaussianStencil sigma r =
   makeStencil
-    ext
+    (rToExtent r)
     (\ix@(Z :. x :. y) ->
-       if isInside2 ext ix
-          then Just (recip (pi * s2) *
-                     exp (-(fromIntegral x ** 2 + fromIntegral y ** 2) /
-                           s2))
+       if inside r ix
+          then Just (gaussian2d sigma x y)
           else Nothing)
+
+gaussian2d :: Double -> Int -> Int -> Double
+gaussian2d sigma x y =
+  (recip (pi * s2) *
+   exp (-(fromIntegral x ** 2 + fromIntegral y ** 2) / s2))
   where s2 = 2 * sigma ** 2
-        ext = ix2 r r
+{-# INLINE gaussian2d #-}
+
+gaussianFilter :: Monad m
+               => Stencil DIM2 Double
+               -> Array U DIM2 Double
+               -> m (Array U DIM2 Double)
+gaussianFilter stencil im = computeP (mapStencil2 BoundClamp stencil im)
+
+applyGaussian :: IO ()
+applyGaussian =
+  applyFilter
+    (\im ->
+       do let (r,g,b) = U.unzip3 im
+          U.zip3 <$>
+            gaussianFilter st r <*>
+            gaussianFilter st g <*>
+            gaussianFilter st b)
+    "XING_B24.BMP"
+    "XING_B24_BLUR.BMP"
+  where st = gaussianStencil 0.84 3
+
+-- applyGreyscale :: IO ()
+-- applyGreyscale =
+--   applyFilter
+--     (\im ->
+--        do gim <- greyscale im
+--           return (U.zip3 gim gim gim))
+--     "FLAG_B24.BMP"
+--     "FLAG_B24_GREY.BMP"
