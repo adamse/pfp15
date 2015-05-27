0e30ff35fc06a23d4b10005deb9911932527cb0b
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-05-27 14:07:49 +0200

    add codez

--- d/page_rank.erl
+++ d/page_rank.erl
@@ -13,22 +13,18 @@ reduce(Url, Ns) ->
 
 % 362.713768
 page_rank() ->
-    dets:open_file(web, [{file, ?DATABASE}]),
     Urls = dets:foldl(fun({K,_},Keys) ->
                               [K|Keys] end,[],web),
     Res = map_reduce:map_reduce_seq(fun map/2, fun reduce/2,
                               [{Url,ok} || Url <- Urls]),
-    dets:close(web),
     Res.
 
 % 195.89102
 page_rank_par() ->
-    dets:open_file(web, [{file, ?DATABASE}]),
     Urls = dets:foldl(fun({K,_},Keys) ->
                               [K|Keys] end,[],web),
     Res = map_reduce:map_reduce_par(fun map/2, 32, fun reduce/2, 32,
                               [{Url,ok} || Url <- Urls]),
-    dets:close(web),
     Res.
 
 % 197,133911
